28d3f5a6197a153edaf8573c5b33831c76671bd3
dani09 <daniel@dani09.de>
2018-07-09 12:42:15 +0200

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    var kotlinVersion: String by extra
+    kotlinVersion = "1.2.31"
+
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(kotlin("gradle-plugin", kotlinVersion))
+    }
+}
+
+plugins {
+    java
+}
+
+group = "de.dani09"
+version = "1.0-SNAPSHOT"
+
+apply {
+    plugin("kotlin")
+}
+
+val kotlinVersion: String by extra
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8", kotlinVersion))
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
