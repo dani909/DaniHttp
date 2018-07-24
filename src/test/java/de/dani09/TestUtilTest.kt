331622ccdd9a9923727ea2bb99103eb89826a90b
dani09 <daniel@dani09.de>
2018-07-24 18:42:44 +0200

    Fixing Test Error with custom non-domain HttpBinUrl

--- src/test/java/de/dani09/TestUtilTest.kt
+++ src/test/java/de/dani09/TestUtilTest.kt
@@ -108,7 +108,7 @@ class TestUtilTest {
     @Test
     fun getHttpBinUrl() {
         assert(TestUtil.getHttpBinUrl().matches(Regex(
-                "https?://.+\\.[a-z]{2,3}"
+                "https?://.+"
         )))
     }
 
