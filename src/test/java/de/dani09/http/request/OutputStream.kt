6ebeaa869bddd5457b157d2db29344d5bc70ce6e
dani09 <daniel@dani09.de>
2018-08-24 16:25:50 +0200

    Added 1MB test file tests

--- src/test/java/de/dani09/http/request/OutputStream.kt
+++ src/test/java/de/dani09/http/request/OutputStream.kt
@@ -1,6 +1,7 @@
 package de.dani09.http.request
 
 import de.dani09.TestUtil.assert
+import de.dani09.dani09de
 import de.dani09.http.Http
 import de.dani09.httpBin
 import org.junit.Test
@@ -30,5 +31,16 @@ class OutputStream {
                 .response
 
         assert(byteArrayOf().toList(), responseArray.toList(), "OutputStreamResponseIsEmpty")
+    }
+
+    @Test
+    fun testFile1MB() {
+        val stream = ByteArrayOutputStream()
+        Http.get("$dani09de/testFiles/1MB.txt")
+                .setOutputStream(stream)
+                .handleRedirects(10)
+                .execute()
+
+        assert(1 * 1024 * 1024, stream.size(), "1MBTestStreamSize")
     }
 }
--- src/test/java/de/dani09/http/request/SimpleRequests.kt
+++ src/test/java/de/dani09/http/request/SimpleRequests.kt
@@ -1,6 +1,7 @@
 package de.dani09.http.request
 
 import de.dani09.TestUtil.assert
+import de.dani09.dani09de
 import de.dani09.http.Http
 import de.dani09.httpBin
 import org.junit.Test
@@ -25,4 +26,12 @@ class SimpleRequests {
         assert(404, response.responseCode, "404 ResponseCode")
     }
 
+    @Test
+    fun testFile1MB() {
+        val response = Http.get("$dani09de/testFiles/1MB.txt")
+                .handleRedirects(10)
+                .execute()
+
+        assert(1 * 1024 * 1024, response.response.size, "1MBTestFileSize")
+    }
 }
