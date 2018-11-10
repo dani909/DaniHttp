819f61be62acb8fb19824a54f4e87e7f3304320d
dani09 <daniel@dani09.de>
2018-11-10 19:29:16 +0100

    Add ability to set the read buffer size

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jfrog.build.extractor.clientConfiguration.ArtifactoryClientConfigurat
 import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
 
 group = "de.dani09"
-version = "0.4.1"
+version = "0.4.2"
 
 buildscript {
     var kotlinVersion: String by extra
